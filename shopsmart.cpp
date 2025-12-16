61f306665ee3fc6769a51cd6d5a1c68ba62910a7
naikalam07 <naikalamakhss0007@gmail.com>
2025-12-17 01:10:40 +0500

    Update shopsmart.cpp

--- shopsmart.cpp
+++ shopsmart.cpp
@@ -5,7 +5,7 @@
 #include <fstream>
 using namespace std;
 
-/* ================= USER MANAGEMENT (LINKED LIST) ================= */
+/* ================ USER MANAGEMENT (LINKED LIST) ================= */
 
 struct User {
     string username, password;
